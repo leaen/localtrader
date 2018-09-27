ca878be23330d5bf84c4e75a67ed7bc5f6ab0f7f
leaen <mossbanay@gmail.com>
2018-09-27 10:21:58 +1000

    Added partially filled to OrderStatus and documentation for Order class

--- localtrader/order.py
+++ localtrader/order.py
@@ -7,7 +7,8 @@ from .side import Side
 class OrderStatus(Enum):
     ACTIVE = 0
     FILLED = 1
-    CANCELLED = 1
+    PARTIALLY_FILLED = 2
+    CANCELLED = 3
 
 
 class Order:
@@ -38,6 +39,7 @@ class Order:
             raise ValueError('Order side must be Side.BUY or Side.SELL')
 
         self.price = price
+        self.total_size = size
         self.size = size
         self.side = side
         self.client_id = client_id
@@ -75,13 +77,40 @@ class Order:
         return self.order_time < other.order_time
 
     def set_status(self, status):
+        """
+        Set the current status of the order.
+        """
+
         if not isinstance(status, OrderStatus):
             raise ValueError('Status must be of type OrderStatus')
 
         self.status = status
 
     def get_status(self):
+        """
+        Get the current status of the order.
+        """
+
         return self.status
 
     def cancel(self):
+        """
+        Cancel the order.
+        """
+
         self.status = OrderStatus.CANCELLED
+
+    def fill(self, fill_size):
+        """
+        Updates the status of the order when some or all of it
+        has been matched.
+        """
+
+        assert(self.status != OrderStatus.CANCELLED)
+
+        self.size -= fill_size
+
+        if self.size == 0:
+            self.status = OrderStatus.FILLED
+        elif 0 < self.size < self.total_size:
+            self.status = OrderStatus.PARTIALLY_FILLED
